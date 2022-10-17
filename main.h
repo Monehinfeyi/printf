a07dcaaee9405a658237419229cb15f943605fe1
Monehin <monehinfeyisara.d@gmail.com>
2022-10-17 09:39:40 -0700

    Prototypes for the project

--- main.h
+++ main.h
@@ -21,4 +21,16 @@ int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
 
+/**
+ * struct code_format - Struct format
+ * @sc: The specifiers
+ * @f: The function associated
+ */
+
+typedef struct code_format
+{
+char *sc;
+int (*f)(va_list);
+} code_f;
+
 #endif/*MAIN_H*/
