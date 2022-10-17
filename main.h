7acbf13d1d308976e18293652bb360f44de2c00e
Monehin <monehinfeyisara.d@gmail.com>
2022-10-17 09:35:19 -0700

    protypes for the project

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
+
+#endif/*MAIN_H*/
