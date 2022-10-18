312e8e8b6448982210d67df4f6ace19d278d394c
Monehin <monehinfeyisara.d@gmail.com>
2022-10-18 07:13:06 -0700

    updated

--- 0-printf.c
+++ 0-printf.c
@@ -14,7 +14,6 @@ int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
-
 if (format == NULL)
 return (-1);
 va_start(list, format);
